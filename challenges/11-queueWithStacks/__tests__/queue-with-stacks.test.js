18867f083bd3e1c9ae7a1b8f16a3cdb9d69bada0
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-20 13:53:02 -0800

    pause

--- challenges/11-queueWithStacks/__tests__/queue-with-stacks.test.js
+++ challenges/11-queueWithStacks/__tests__/queue-with-stacks.test.js
@@ -20,31 +20,3 @@ describe('Queue with Stacks Tests', () => {
     expect(pseudoQueue.stack2).toBeDefined();
   });
 });
-
-  // it('Can successfully push onto a stack', () => {
-  //   pseudoQueue.enqueue(2);
-  //   expect(pseudoQueue.stack1.peek()).toEqual(2);
-  // });
-
-  // it('Can successfully push multiple nodes onto a stack', () => {
-  //   pseudoQueue.enqueue(2);
-  //   pseudoQueue.enqueue(12);
-  //   pseudoQueue.enqueue(22);
-  //   expect(pseudoQueue.stack1.peek()).toEqual(22);
-  // });
-
-  // it('Can successfully dequeue one val', () => {
-  //   pseudoQueue.enqueue(12);
-  //   pseudoQueue.enqueue(22);
-  //   expect(pseudoQueue.dequeue()).toEqual(12);
-  //   expect(pseudoQueue.stack2.top.val).toEqual(22);
-  // });
-
-  // it('Can successfully dequeue multiple vals', () => {
-  //   pseudoQueue.enqueue(12);
-  //   pseudoQueue.enqueue(22);
-  //   pseudoQueue.enqueue(32);
-  //   expect(pseudoQueue.dequeue()).toEqual(12);
-  //   expect(pseudoQueue.dequeue()).toEqual(22);
-  //   expect(pseudoQueue.dequeue()).toEqual(32);
-  // });
