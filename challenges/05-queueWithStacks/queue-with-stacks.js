f5d7116c79234344330fa1e9d5369ca32b546e94
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-20 12:54:15 -0800

    node, stack, and pseudoqueue classes

---
+++ challenges/05-queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,63 @@
+use strict';
+
+class Node {
+  constructor (value) {
+    this.val = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor () {
+    this.top = null;
+    this.length = 0;
+  }
+
+  push (value) {
+    if(!value) return false;
+    let node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+    this.length++;
+  }
+
+  pop(){
+    let temp = this.top.value;
+    this.top = this.top.next;
+    this.length--;
+    return temp;
+  }
+
+  peek () {
+    if(!this.top){
+      return null;
+    } else {
+      rturn this.top.value;
+    }
+  }
+}
+ 
+class PseudoQueue{
+  constructor(){
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+  
+  enqueue(value){
+    this.stack1.push(value);
+  }
+
+  dequeue(){
+    if(this.stack2.length === 0 {
+      if(this.stack1.length === 0) return 'Empty Queue';
+      while(this.stack1.length > 0){
+        let p = this.stack1.pop();
+        this.stack2.push(p);
+      }
+    }
+    return this.stack2.pop();
+  }
+}
+
+module.exports = PseudoQueue;
+
