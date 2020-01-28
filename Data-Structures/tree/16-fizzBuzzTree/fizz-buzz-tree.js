1f4fa02423664fa6275c00f0acc909469cbd2e5c
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-27 23:17:06 -0800

    FizzBuzz and Recursion

---
+++ Data-Structures/tree/16-fizzBuzzTree/fizz-buzz-tree.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Node value is changed, based upon modular operator
+ * @param {*} value
+ */
+function fizzBuzz(value) {
+  if (value % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (value % 5 === 0) {
+    return 'Buzz';
+  } else if (value % 3 === 0) {
+    return 'Fizz';
+  } else {
+    return value;
+  }
+}
+
+/**
+ * Recusion to evalue tree node value
+ * @param {object} tree
+ */
+function fizzBuzzTree(tree) {
+  if (!tree) {
+    return 'No Tree';
+  }
+  let recursive = node => {
+    if (node.left) recursive(node.left);
+    node.value = fizzBuzz(node.value);
+  };
+  recursive(tree.root);
+  return tree;
+}
+
+module.exports = fizzBuzzTree;
