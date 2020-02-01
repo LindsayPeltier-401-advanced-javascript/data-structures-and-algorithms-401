19cdeae65c0435d246528ceeac7211d7377c1efa
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-31 16:24:31 -0800

    updated top r readme

--- Data-Structures/stacksAndQueues/queueWithStacks/__tests__/queue-with-stacks.test.js
+++ Data-Structures/stacksAndQueues/queueWithStacks/__tests__/queue-with-stacks.test.js
@@ -1,22 +1,44 @@
 'use strict';
 
-const QueueWithStacks = require('../queue-with-stacks.js');
+const pseduoQueue = require('../queue-with-stacks');
 
-
-describe('Queue with Stacks Tests', () => {
-  let pseudoQueue;
+describe('Queue with stacks', () => {
+  let queue;
 
   beforeEach(() => {
-    pseudoQueue = new QueueWithStacks();
+    queue = new pseduoQueue;
   });
 
-  it('should actually work', () => {
-    expect(pseudoQueue).toBeDefined();
-    expect(pseudoQueue).toBeInstanceOf(QueueWithStacks);
+  describe('Instance of', () => {
+    it('should be instance of a new pseduoQueue', () => {
+      expect(queue).toBeInstanceOf(pseduoQueue);
+    });
   });
 
-  it('should create two stacks', () => {
-    expect(pseudoQueue.stack1).toBeDefined();
-    expect(pseudoQueue.stack2).toBeDefined();
+  describe('enqueue(value', () => {
+    it('should have one value', () => {
+      queue.enqueue(5);
+      expect(queue.stack1.peek().value).toBe(5);
+    });
+
+    it('should be able to hold multiple values', () => {
+      queue.enqueue(5);
+      queue.enqueue(6);
+      queue.enqueue(7);
+      expect(queue.stack1.peek().value).toBe(7);
+    });
+
+  });
+  describe('dequeue', () => {
+    it('should pop the first element', () => {
+      queue.enqueue(5);
+      queue.enqueue(6);
+      queue.enqueue(7);
+      let result = queue.dequeue();
+      console.log(result);
+      expect(result.value.value).toBe(5);
+    });
   });
 });
+
+
